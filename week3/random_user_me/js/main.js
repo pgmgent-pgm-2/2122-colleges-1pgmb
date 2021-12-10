25c7c852cfbd689e7d77dc941ab4453f5077b7cf
drdynscript <drdynscript@gmail.com>
2021-12-10 20:02:15 +0100

    Added fetch, promise and async / await

--- week3/random_user_me/js/main.js
+++ week3/random_user_me/js/main.js
@@ -30,20 +30,98 @@ const RANDOM_USER_ME_API = `https://randomuser.me/api/?results=100`;
       //     console.log(error);
       //   });
 
-        getJSONByPromise(RANDOM_USER_ME_API)
-          .then((data) => {
-            const users = data.results;
-            this.$usersList.innerHTML = users.map((user) => {
-            return `
-            <article>
-              <h3>${user.name.first} ${user.name.last}</h3>
-            </article>
-            `;
-            }).join('');
-          })
-          .catch((error) => {
+        // getJSONByPromise(RANDOM_USER_ME_API)
+        //   .then((data) => {
+        //     const users = data.results;
+        //     this.$usersList.innerHTML = users.map((user) => {
+        //     return `
+        //     <article>
+        //       <h3>${user.name.first} ${user.name.last}</h3>
+        //     </article>
+        //     `;
+        //     }).join('');
+        //   })
+        //   .catch((error) => {
 
-          });
+        //   });
+
+        // 3. Fetching data with fetch
+        // const rumPromise = fetch(RANDOM_USER_ME_API, {});
+        // rumPromise
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     const users = data.results;
+        //     this.$usersList.innerHTML = users.map((user) => {
+        //     return `
+        //     <article>
+        //       <h3>${user.name.first} ${user.name.last}</h3>
+        //     </article>
+        //     `;
+        //     }).join('');
+        //   })
+        //   .catch((error) => console.log(error));
+        this.asyncLoadExternalData();
+
+        this.asyncManipulateExternalDataFromPlaceholderAPI();
+    },
+    async asyncLoadExternalData() {
+      try {
+        const response = await fetch(RANDOM_USER_ME_API, {});
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }      
+    },
+    async asyncManipulateExternalDataFromPlaceholderAPI() {
+      try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        let data = await response.json();
+        console.log(data);
+
+        response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`);
+        data = await response.json();
+        console.log(data);
+
+        data = {
+          title: 'Graduaat Programmeren',
+          body: 'Like Graphics Love Code Make Cool Stuff',
+          userId: 3
+        };
+
+        response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+          method: 'POST',
+          headers: { 'Content-type': 'application/json; charset=UTF-8'},
+          body: JSON.stringify(data)
+        });
+        data = await response.json();
+
+        data = {
+          id: 1,
+          title: 'Graduaat Programmeren',
+          body: 'Super like Graphics Love Code Make Cool Stuff',
+          userId: 3
+        };
+        response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`, {
+          method: 'PUT',
+          headers: { 'Content-type': 'application/json; charset=UTF-8'},
+          body: JSON.stringify(data)
+        });
+        data = await response.json();
+
+        response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`, {
+          method: 'DELETE'
+        });
+        data = await response.json();
+        console.log(data);
+
+        if (true) {
+         throw new Error('IT IS BROKEN'); 
+        }
+
+      } catch(error) {
+        console.error(error);
+      }
     }
   }
   app.init();
