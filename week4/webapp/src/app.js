3f57b79d58c89665abcd9e429d3e932932ca9b60
drdynscript <drdynscript@gmail.com>
2021-12-17 14:17:34 +0100

    Added my first little server

--- week4/webapp/src/app.js
+++ week4/webapp/src/app.js
@@ -1,1 +1,18 @@
-console.log('APPLICATION STARTED!');
+/*
+Import packages
+*/
+const express = require('express');
+
+/*
+Create an express.js application
+*/
+const app = express();
+
+/*
+Start the server (express application)
+Listen to all incoming requests
+*/
+app.listen(8080, '127.0.0.1', (err) => {
+  if (err) throw err;
+  console.log(`Application started. Server is listening at ${'127.0.0.1'} on port ${8080}.`);
+});
